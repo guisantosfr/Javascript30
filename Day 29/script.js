e0d262939adb8cfa3d302c3842173bd142ade00c
Tanmaykk <tanmaykachroo@gmail.com>
2021-10-02 04:20:26 +0530

    Added alert when timer hits 0

--- Day 29/script.js
+++ Day 29/script.js
@@ -1,25 +1,29 @@
 let countdown;
 
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
-const buttons = document.querySelectorAll('[data-time]');
+const buttons = document.querySelectorAll("[data-time]");
+var audio = new Audio("alert.mp3");
 
-function timer(seconds){
+function timer(seconds) {
   //clear any other existing timers.
   clearInterval(countdown);
 
   const now = Date.now();
   const then = now + seconds * 1000;
-  
+
   displayTimeLeft(seconds);
   displayEndTime(then);
 
   countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now())/1000);
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
 
     //check if we should stop it!
-    if(secondsLeft < 0){
+    if (secondsLeft === 0) {
+      audio.play();
+    }
+    if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
@@ -28,34 +32,38 @@ function timer(seconds){
   }, 1000);
 }
 
-function displayTimeLeft(seconds){
+function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
 
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
 
-function displayEndTime(timestamp){
+function displayEndTime(timestamp) {
   const end = new Date(timestamp);
 
   const hour = end.getHours();
   const minutes = end.getMinutes();
 
-  endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = `Be Back At ${hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes}`;
   //use `hour > 12 ? hour - 12 : hour`
   //to get 3:00 instead of 15:00, for example
 }
 
-function startTimer(){
+function startTimer() {
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
 }
 
-buttons.forEach(button => button.addEventListener('click', startTimer));
+buttons.forEach((button) => button.addEventListener("click", startTimer));
 
-document.customForm.addEventListener('submit', function(e){
+document.customForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const mins = this.minutes.value;
   timer(mins * 60);
